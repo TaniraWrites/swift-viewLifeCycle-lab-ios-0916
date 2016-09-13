7df72bf77e728bacd27630786e60f23101dfc0a9
Taniraw <tanirawrites@gmail.com>
2016-09-13 16:10:44 -0400

    View Life Cycle Lab Completed

--- FunWithViews/ViewController.swift
+++ FunWithViews/ViewController.swift
@@ -133,13 +133,13 @@ class ViewController: UIViewController {
         }
     
     
-        
-    }
+    
+    
     
     // Returns back a random Int (1, 2, 3, 4, 5, or 6)
     func randomDiceRoll() -> Int {
         return Int(arc4random_uniform(6) + 1)
     }
- 
-}
 
+
+}
